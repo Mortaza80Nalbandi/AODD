b4a6dcaa308d7e6b2f403995fc47f12a505ff63e
Mortaza Nalbandi <mortazana8@gmail.com>
2023-02-23 21:30:13 +0330

    characters added

---
+++ Assets/scripts/Character.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    int health;
+    int damage;
+    float speed = 0.06f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetKey(KeyCode.D))
+        {
+           transform.Translate (speed, 0f, 0f); 
+        }else if (Input.GetKey(KeyCode.A))
+        {
+           transform.Translate (-1*speed, 0f, 0f); 
+        }
+    }
+}
