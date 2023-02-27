d2ae0f1155ca5e2a2fe817f15d035ed1a5257a49
Mortaza Nalbandi <mortazana8@gmail.com>
2023-02-27 14:05:05 +0330

    The bases are created and theirscripts are started

--- Assets/scripts/EnemyAI.cs
+++ Assets/scripts/EnemyAI.cs
@@ -9,15 +9,34 @@ public class EnemyAI : MonoBehaviour
     List<EnemyArcher> enemyArchers ;
     List<swordsman> swordmans ;
     List<Shooter> shooters ;
+    
+    float spawnRate ;
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemies = new List<Enemy>();
+        tanks = new List<Tank>();
+        enemyArchers = new List<EnemyArcher>();
+        swordmans = new List<swordsman>();
+        shooters = new List<Shooter>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(spawnRate<0){
+            if(enemies.Count<=10){
+                spawnUnit("Enemy");
+            }else if(enemyArchers.Count<=5){
+                spawnUnit("EnemyArcher");
+            }else if(tanks.Count<=2){
+                spawnUnit("Tank");
+            }
+        }
         
     }
+    void spawnUnit(string type){
+       int x=0;
+    }
+    
 }
